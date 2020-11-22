43b1e0ede3bd0498cf899e97cfd459d01dfb7f02
John Garrett <jacgarrett18@gmail.com>
2020-11-22 15:05:28 -0500

    start with selecting points

--- script.py
+++ script.py
@@ -7,18 +7,26 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 fontColor = (255, 255, 255)
 fontThickness = 2
 
-def skew_frame(frame):
-    print(frame)
+def on_click(event, x, y, p1, p2):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x)
+        print(y)
+        print(p1)
+        print(p2)
+
 
 roi = None
 while(cap.isOpened()):
     rect, current_frame = cap.read()
     if roi is None:
         roi = cv2.selectROI("roi pane", current_frame, False)
+        cv2.imshow("image", current_frame)
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image', on_click)
         print(roi)
 
     cropped_frame = current_frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
-    skewed_frame = skew_frame(current_frame)
+    skewed_frame = current_frame
 
     cv2.putText(current_frame, 'input stream', (200, 90), font, fontThickness, fontColor, 5)
     cv2.imshow("Current frame", current_frame)
